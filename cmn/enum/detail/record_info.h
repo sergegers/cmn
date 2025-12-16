cfa33704ddd59d9b3d4472e2856f66bf4bafe26f
Serge Kork <menusman@gmail.com>
2025-12-16 04:18:41 +0300

    - drop cmn::copy() function

--- cmn/enum/detail/record_info.h
+++ cmn/enum/detail/record_info.h
@@ -42,15 +42,6 @@ struct record_info
         m_wname{ int_<En_>{}, enum_member_wname }
     {}
 
-    //-----------------------------------------------------------------------------
-    template <typename Char, typename CharTraits = std::char_traits<Char>>
-    constexpr auto &name() const noexcept
-    {
-        if constexpr (std::is_same_v<Char, char>) return m_name;
-        else if constexpr (std::is_same_v<Char, wchar_t>) return m_wname;
-        else static_assert(!std::is_same_v<Char, Char>, "Not implemented");
-    }
-
     template <typename Char, typename CharTraits>
     constexpr auto &name(std::basic_ios<Char, CharTraits> const &) const noexcept
     {
@@ -75,6 +66,15 @@ struct record_info
     constexpr auto operator == (record_info const &other) const noexcept
     {
         return (*this <=> other) == std::strong_ordering::equivalent;
+    }
+
+    //-----------------------------------------------------------------------------
+    template <typename Char, typename CharTraits = std::char_traits<Char>>
+    constexpr auto &name() const noexcept
+    {
+        if constexpr (std::is_same_v<Char, char>) return m_name;
+        else if constexpr (std::is_same_v<Char, wchar_t>) return m_wname;
+        else static_assert(!std::is_same_v<Char, Char>, "Not implemented");
     }
 };
 
--- cmn/enum/io/out.h
+++ cmn/enum/io/out.h
@@ -1,30 +1,53 @@
 #pragma once
 
-#include <cmn/enum/io/format_traits.h>
+#include <cmn/meta/concepts.h>
+#include <cmn/meta/traits.h>
+#include <cmn/util/feature.h>
 
-namespace cmn::enum_::io
+#include <cmn/enum/detail/record_info.h>
+
+#include "print.h"
+
+namespace cmn::enum_
 {
 
 namespace detail
+{
+
+// keep out operations here to avoid circular dependencies due to print_t
+template <c::adapted_enum E, typename Char, typename CharTraits, typename OutIt>
+constexpr auto out(record_info<E> const &rec, c::list_source_options<Char, CharTraits> auto const& options) -> void
+{
+    using enum io::print_t;
+
+    io::print_t po = options.options;
+    if (has_feature(po, ns))
+    {
+        
+    }
+}
+
+}
+
+namespace io::detail
 {
 
 ///////////////////////////////////////////////////////////////////////////////
 ///
 /// redirect to appropriate out_...() function
 ///
-template <typename Char, typename CharTraits, c::adapted_enum E, typename OutIt>
-constexpr auto out(int_<kind_t::enum_>, E en, vsource_options<Char, CharTraits> const & options,
-    OutIt out_it) -> void
+///////////////////////////////////////////////////////////////////////////////
+
+template <c::adapted_enum E, typename Char, typename CharTraits, typename OutIt>
+constexpr auto out(int_<kind_t::enum_>, E en, c::list_source_options<Char, CharTraits> const &options, OutIt out_it) -> void
 {}
 
-template <typename Char, typename CharTraits, c::adapted_enum E, typename OutIt>
-constexpr auto out(int_<kind_t::bitfield>, E en, vsource_options<Char, CharTraits> const& options,
-    OutIt out_it) -> void
+template <c::adapted_enum E, typename Char, typename CharTraits, typename OutIt>
+constexpr auto out(int_<kind_t::bitfield>, E en, c::list_source_options<Char, CharTraits> const &options, OutIt out_it) -> void
 {}
 
-template <typename Char, typename CharTraits, c::adapted_enum E, typename OutIt>
-constexpr auto out(int_<kind_t::combo>, E en, vsource_options<Char, CharTraits> const &options,
-    OutIt out_it) -> void
+template <c::adapted_enum E, typename Char, typename CharTraits, typename OutIt>
+constexpr auto out(int_<kind_t::combo>, E en, c::list_source_options<Char, CharTraits> const &options, OutIt out_it) -> void
 {}
 
 
